c0e84d22303de56cc0b0733e54605b0c386aa972
amulyaprasanth <amulyaprasanth301@gmail.com>
2025-03-19 23:14:15 +0530

    added dynamic urls in development and production

--- frontend/src/components/CardList.tsx
+++ frontend/src/components/CardList.tsx
@@ -1,18 +1,21 @@
 import {Card} from "./Card.tsx";
 import ScrollAnimation from "react-animate-on-scroll";
 import "animate.css/animate.compat.css";
+import {config} from "../Constants.ts";
+
+const url = config.url.BASE_URL;
 
 export const CardList = () => {
     const cards = [
         {
             title: "WikiArXiv Explore",
             description: "Effortlessly retrieve and explore knowledge from Wikipedia and ArXiv.",
-            link: "/tool"
+            link: `${url}/tool`
         },
         {
             title: "DocQnA",
             description: "Unlock answers directly from your PDFs with ease.",
-            link: "/rag"
+            link: `${url}/rag`
         }
     ];
 
