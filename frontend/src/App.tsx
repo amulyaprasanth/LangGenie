9f5ebe084cb686d55c9c3e075bcb63f51bf96339
amulyaprasanth <amulyaprasanth301@gmail.com>
2025-05-31 19:13:49 +0530

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,12 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {Navbar} from './components/Navbar.tsx';
 import {Home} from './pages/Home.tsx';
-import {ToolAgent} from './pages/ToolAgent';
-import {RagChain} from './pages/RagChain';
 import Footer from "./components/Footer.tsx";
 
 function App() {
     return (
         <div>
             <Navbar/>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/tool" element={<ToolAgent/>}/>
-                    <Route path="/rag" element={<RagChain/>}/>
-                </Routes>
-            </Router>
+            <Home/>
             <Footer/>
         </div>
     );
